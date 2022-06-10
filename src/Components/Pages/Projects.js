c24f86e8a4b383a2304685b2f9b2c949e0fbafb5
irazramin <irazramin@gmail.com>
2022-06-10 20:32:54 +0600

    dark mode section added in project details

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -116,22 +116,34 @@ const handleProjectDetails = (id) => {
         role='dialog'
       >
         <div class='modal-dialog modal-lg relative w-auto pointer-events-none'>
-          <div class='modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current'>
-            <div class='modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md'>
+          <div class='modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-clip-padding rounded-md outline-none text-current'>
+            <div
+              class={`${
+                modeOn === 'true'
+                  ? 'bg-slate-900 text-white'
+                  : 'bg-white text-slate-900'
+              } modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md`}
+            >
               <h5
-                class='text-xl font-medium leading-normal text-gray-800'
+                class='text-xl font-medium leading-normal '
                 id='exampleModalLgLabel'
               >
                 {projectDetails.projectName} Details
               </h5>
               <button
                 type='button'
-                class='btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline'
+                class='btn-close box-content w-4 h-4 p-1  border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100  hover:opacity-75 hover:no-underline'
                 data-bs-dismiss='modal'
                 aria-label='Close'
               ></button>
             </div>
-            <div class='modal-body relative p-4'>
+            <div
+              class={`${
+                modeOn === 'true'
+                  ? 'bg-slate-900 text-white'
+                  : 'bg-white text-slate-900'
+              }  modal-body relative p-4`}
+            >
               <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5'>
                 {projectDetails?.img?.map((img) => (
                   <img src={img} alt='' />
